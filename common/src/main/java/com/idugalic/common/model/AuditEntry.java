798df600f8ab950bd69938539008dec2dec97aa7
Ivan Dugalic <idugalic@gmail.com>
2016-04-05 00:02:47 +0200

    Initial structure of the project (maven)

---
+++ common/src/main/java/com/idugalic/common/model/AuditEntry.java
@@ -0,0 +1,50 @@
+package com.idugalic.common.model;
+
+import java.util.Date;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+public class AuditEntry {
+	private String who;
+	private Date when;
+
+	public AuditEntry(String who, Date when) {
+		super();
+		this.who = who;
+		this.when = when;
+	}
+
+	public String getWho() {
+		return who;
+	}
+
+	public void setWho(String who) {
+		this.who = who;
+	}
+
+	public Date getWhen() {
+		return when;
+	}
+
+	public void setWhen(Date when) {
+		this.when = when;
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		return EqualsBuilder.reflectionEquals(this, o);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+}
