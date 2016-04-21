2e4eaf4789b9e4398f9c5ac8f4bae395545e3643
Ivan Dugalic <idugalic@gmail.com>
2016-04-21 20:14:46 +0200

    New queries added in repository of blogqeryside

--- query-side-blog/src/main/java/com/idugalic/queryside/blog/repository/BlogPostRepository.java
+++ query-side-blog/src/main/java/com/idugalic/queryside/blog/repository/BlogPostRepository.java
@@ -1,13 +1,43 @@
 package com.idugalic.queryside.blog.repository;
 
+import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import com.idugalic.common.blog.model.BlogPostCategory;
 import com.idugalic.queryside.blog.domain.BlogPost;
 
 @RepositoryRestResource(collectionResourceRel = "blogposts", path = "blogposts")
 public interface BlogPostRepository extends ReadOnlyPagingAndSortingRepository {
-	public List<BlogPost> findByDraft(@Param("draft") Boolean draft);
+	Page<BlogPost> findByDraftTrue(Pageable pageRequest);
+
+	Page<BlogPost> findByCategoryAndDraftFalse(@Param("category") BlogPostCategory category, Pageable pageable);
+
+	Page<BlogPost> findByDraftFalseAndPublishAtBeforeOrderByPublishAtDesc(@Param("publishedBefore") Date publishedBefore, Pageable pageRequest);
+
+	Page<BlogPost> findByCategoryAndDraftFalseAndPublishAtBefore(@Param("category") BlogPostCategory category, @Param("publishedBefore")  Date publishedBefore, Pageable pageRequest);
+
+	Page<BlogPost> findByBroadcastAndDraftFalseAndPublishAtBefore(@Param("broadcast") boolean broadcast, @Param("publishedBefore")  Date publishedBefore, Pageable pageRequest);
+
+	Page<BlogPost> findByDraftFalseAndPublishAtAfter(@Param("now") Date now, Pageable pageRequest);
+
+	Page<BlogPost> findByDraftFalseAndAuthorIdAndPublishAtBeforeOrderByPublishAtDesc(@Param("authorId") String authorId, @Param("publishedBefore") Date publishedBefore, Pageable pageRequest);
+
+	@Query("select p from BlogPost p where YEAR(p.publishAt) = ?1 and MONTH(p.publishAt) = ?2 and DAY(p.publishAt) = ?3")
+	Page<BlogPost> findByDateYearMonthDay(@Param("year")int year, @Param("month")int month, @Param("day")int day, Pageable pageRequest);
+
+	@Query("select p from BlogPost p where YEAR(p.publishAt) = ?1 and MONTH(p.publishAt) = ?2")
+	Page<BlogPost> findByDateYearMonth(@Param("year")int year, @Param("month")int month, Pageable pageRequest);
+
+	@Query("select p from BlogPost p where YEAR(p.publishAt) = ?1")
+	Page<BlogPost> findByDateYear(@Param("year")int year, Pageable pageRequest);
+	
+	BlogPost findByTitle(@Param("title") String title);
+
+	BlogPost findByPublicSlugAndDraftFalseAndPublishAtBefore(@Param("publicSlug") String publicSlug, @Param("now") Date now);
 }
