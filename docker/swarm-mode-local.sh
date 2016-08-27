88b030c9a6b8d6560f5d89b8c7193355483a0fa3
Ivan Dugalic <idugalic@gmail.com>
2016-08-27 22:54:12 +0200

    Cleaning up the swarm mode shell script

--- docker/swarm-mode-local.sh
+++ docker/swarm-mode-local.sh
@@ -22,24 +22,39 @@ docker-machine create \
 docker $(docker-machine config swmaster) swarm init \
 	--advertise-addr $(docker-machine ip swmaster):2377
 
-# CHECK TOKEN 
+# CHECK TOKEN returned in output of the INIT command!!!!!!
 
 # Configure swarm mode cluster - join nodes
 docker $(docker-machine config swnode1) swarm join --token SWMTKN-1-65kph7zg7e7rbyr1siz4ecguq8m9b9xppdw8w3eo13j2smwuer-0100l08ehvk4ogump94fcyluk $(docker-machine ip swmaster):2377
 docker $(docker-machine config swnode2) swarm join --token SWMTKN-1-65kph7zg7e7rbyr1siz4ecguq8m9b9xppdw8w3eo13j2smwuer-0100l08ehvk4ogump94fcyluk $(docker-machine ip swmaster):2377
 docker $(docker-machine config swnode3) swarm join --token SWMTKN-1-65kph7zg7e7rbyr1siz4ecguq8m9b9xppdw8w3eo13j2smwuer-0100l08ehvk4ogump94fcyluk $(docker-machine ip swmaster):2377
+
 # Create Bundle from compose file
-docker-compose bundle -o micro-company.dsb
+#docker-compose bundle -o micro-company.dsb
 # Point your local docker client to the swarm master 
 eval $(docker-machine env swmaster)
+#List all nodes
+docker node ls
 # Create a stack using docker deploy command
 docker deploy --file micro-company.dsb micro-company
 # List all services
 docker service ls
 # Inspect task. Please check the PublishedPort and use it to test the API latter on.
 docker service inspect micro-company_api-gateway
-# CURL. Please note to use PublishedPort instead of 30000
-# curl $(docker-machine ip swmaster):30000/info
-# curl -H "Content-Type: application/json" -X POST -d '{"title":"xyz","rawContent":"xyz","publicSlug": "publicslug","draft": true,"broadcast": true,"category": "ENGINEERING", "publishAt": "2016-12-23T14:30:00+00:00"}' $(docker-machine ip swmaster):30000/command/blog/blogposts
-# curl $(docker-machine ip swmaster):30000/query/blog/blogposts
-# Visit $(docker-machine ip swmaster):30000/socket/index.html and listen to all events via Web socket.
+# Scale service  'scale micro-company_api-gateway'
+docker idugalic$ docker service scale micro-company_api-gateway=2
+docker service ps micro-company_api-gateway
+# List all services
+docker service ls
+
+# CURL. Please note to use PublishedPort instead of 30006
+# curl $(docker-machine ip swmaster):30006/info
+# curl -H "Content-Type: application/json" -X POST -d '{"title":"xyz","rawContent":"xyz","publicSlug": "publicslug","draft": true,"broadcast": true,"category": "ENGINEERING", "publishAt": "2016-12-23T14:30:00+00:00"}' $(docker-machine ip swmaster):30006/command/blog/blogposts
+# curl $(docker-machine ip swmaster):30006/query/blog/blogposts
+# Visit $(docker-machine ip swmaster):30006/socket/index.html (http://192.168.99.100:30006/socket/index.html) and listen to all events via Web socket.
+
+# Remove machines
+# docker-machine rm swmaster
+# docker-machine rm swnode1
+# docker-machine rm swnode2
+# docker-machine rm swnode3
