3e69b3bfcdc461fd89c128cfd579e8f3fa3fa0ed
Ivan Dugalic <idugalic@gmail.com>
2016-08-27 22:07:26 +0200

    Swarm mode shell script udpdated to match docker version 1.12.1

--- docker/swarm-mode-local.sh
+++ docker/swarm-mode-local.sh
@@ -20,22 +20,25 @@ docker-machine create \
  	swnode3
 # Configure swarm mode cluster - initialization on master
 docker $(docker-machine config swmaster) swarm init \
-	--listen-addr $(docker-machine ip swmaster):2377
+	--advertise-addr $(docker-machine ip swmaster):2377
+
+# CHECK TOKEN 
+
 # Configure swarm mode cluster - join nodes
-docker $(docker-machine config swnode1) swarm join $(docker-machine ip swmaster):2377
-docker $(docker-machine config swnode2) swarm join $(docker-machine ip swmaster):2377
-docker $(docker-machine config swnode3) swarm join $(docker-machine ip swmaster):2377
+docker $(docker-machine config swnode1) swarm join --token SWMTKN-1-65kph7zg7e7rbyr1siz4ecguq8m9b9xppdw8w3eo13j2smwuer-0100l08ehvk4ogump94fcyluk $(docker-machine ip swmaster):2377
+docker $(docker-machine config swnode2) swarm join --token SWMTKN-1-65kph7zg7e7rbyr1siz4ecguq8m9b9xppdw8w3eo13j2smwuer-0100l08ehvk4ogump94fcyluk $(docker-machine ip swmaster):2377
+docker $(docker-machine config swnode3) swarm join --token SWMTKN-1-65kph7zg7e7rbyr1siz4ecguq8m9b9xppdw8w3eo13j2smwuer-0100l08ehvk4ogump94fcyluk $(docker-machine ip swmaster):2377
 # Create Bundle from compose file
 docker-compose bundle -o micro-company.dsb
 # Point your local docker client to the swarm master 
 eval $(docker-machine env swmaster)
 # Create a stack using docker deploy command
-docker deploy -f micro-company.dsb micro-company
+docker deploy --file micro-company.dsb micro-company
 # List all services
 docker service ls
-# Inspect task
+# Inspect task. Please check the PublishedPort and use it to test the API latter on.
 docker service inspect micro-company_api-gateway
-# CURL
+# CURL. Please note to use PublishedPort instead of 30000
 # curl $(docker-machine ip swmaster):30000/info
 # curl -H "Content-Type: application/json" -X POST -d '{"title":"xyz","rawContent":"xyz","publicSlug": "publicslug","draft": true,"broadcast": true,"category": "ENGINEERING", "publishAt": "2016-12-23T14:30:00+00:00"}' $(docker-machine ip swmaster):30000/command/blog/blogposts
 # curl $(docker-machine ip swmaster):30000/query/blog/blogposts
