e552aeee9e85d5ed2b0c3e6270ed90fdc92ba14a
Ivan Dugalic <idugalic@gmail.com>
2016-10-09 20:22:16 +0200

    'Remove' shell scripts created.
    To remove all virtual machines (docker machines)

---
+++ docker/swarm-local-remove.sh
@@ -0,0 +1,8 @@
+# Remove machines
+docker-machine rm -f swl-consul
+docker-machine rm -f swl-demo0
+docker-machine rm -f swl-demo1
+docker-machine rm -f swl-demo2
+docker-machine rm -f swl-demo3
+docker-machine rm -f swl-demo4
+
---
+++ docker/swarm-mode-local-remove.sh
@@ -0,0 +1,5 @@
+# Remove machines
+docker-machine rm -f swmaster
+docker-machine rm -f swnode1
+docker-machine rm -f swnode2
+docker-machine rm -f swnode3
