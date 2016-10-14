c8f49e1327533d348420b7aea4903da5ade5a307
Ivan Dugalic <idugalic@gmail.com>
2016-10-14 20:32:50 +0200

    Switching config server on master branch. Adding tag as project version to docker images

--- circuit-breaker/src/main/java/com/idugalic/circuitbreaker/Application.java
+++ circuit-breaker/src/main/java/com/idugalic/circuitbreaker/Application.java
@@ -2,12 +2,10 @@ package com.idugalic.circuitbreaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableHystrixDashboard
-@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
