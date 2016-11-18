7f7a725507e86bd4298402f1509055d946c8c0cb
Ivan Dugalic <idugalic@gmail.com>
2016-11-18 13:59:14 +0100

    Support for CF introduced

--- command-side-blog-service/src/main/java/com/idugalic/commandside/blog/configuration/CloudConfiguration.java
+++ command-side-blog-service/src/main/java/com/idugalic/commandside/blog/configuration/CloudConfiguration.java
@@ -1,12 +1,19 @@
 package com.idugalic.commandside.blog.configuration;
 
 
+import javax.sql.DataSource;
+
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
 public class CloudConfiguration extends AbstractCloudConfig{
+    
+    @Bean
+    public DataSource dataSource() {
+        return connectionFactory().dataSource();
+    }
     
     @Profile("cloud")
     @Bean()
--- command-side-project-service/src/main/java/com/idugalic/commandside/project/configuration/CloudConfiguration.java
+++ command-side-project-service/src/main/java/com/idugalic/commandside/project/configuration/CloudConfiguration.java
@@ -1,6 +1,8 @@
 package com.idugalic.commandside.project.configuration;
 
 
+import javax.sql.DataSource;
+
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
@@ -8,7 +10,11 @@ import org.springframework.context.annotation.Profile;
 
 public class CloudConfiguration extends AbstractCloudConfig{
     
-  
+    @Bean
+    public DataSource dataSource() {
+        return connectionFactory().dataSource();
+    }
+    
     @Profile("cloud")
     @Bean()
     public ConnectionFactory rabbitFactory() {
