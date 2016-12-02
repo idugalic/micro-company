e1a8ec850c2ab70e1c3dbb0309d58c135d74e538
Ivan Dugalic <idugalic@gmail.com>
2016-12-02 23:47:58 +0100

    Docker Swarm on AWS (Cloud formation)

--- docker/swarm-mode-aws.sh
+++ docker/swarm-mode-aws.sh
@@ -4,30 +4,37 @@ set -e
 
 
 echo "-------------------------"
-echo "### Docker for AWS - beta ###"
+echo "### Docker for AWS - public beta ###"
 echo "-------------------------"
-echo "Step1:  Register for AWS - beta (https://beta.docker.com/docs/)"
-echo "Step2:  Login to your account as a root and create user that will be used latter (http://docs.aws.amazon.com/IAM/latest/UserGuide/id_users_create.html#id_users_create_console)"
-echo "Step3:  Create Your Key Pair Using Amazon EC2. Please not that the key will be downloaded by the browser. In my case it is '/Users/idugalic/.ssh/idugalic.pem' file. (http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-key-pairs.html#having-ec2-create-your-key-pair)"
-echo "Step4:  Create stack on AWS by using CloudFormation - Follow the instructions in the email (http://docker.us13.list-manage.com/track/click?u=761fa9756d4209ea04a811254&id=cd3b21e42e&e=78cc80554c)"
+echo "Step1:  Login to your account as a root and create user that will be used latter (http://docs.aws.amazon.com/IAM/latest/UserGuide/id_users_create.html#id_users_create_console)"
+echo "Step2:  Create Your Key Pair Using Amazon EC2. Please not that the key will be downloaded by the browser. In my case it is '/Users/idugalic/.ssh/idugalic.pem' file. (http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-key-pairs.html#having-ec2-create-your-key-pair)"
+echo "Step3:  Create stack on AWS by using CloudFormation template - https://blog.docker.com/2016/11/docker-aws-public-beta/ (https://console.aws.amazon.com/cloudformation/home#/stacks/new?stackName=Docker&templateURL=https://docker-for-aws.s3.amazonaws.com/aws/beta/aws-v1.13.0-rc2-beta12.json)"
 echo "-----------------------------------------------------------------------------------------------------------------------------"
 
-echo "Find public DNS of one of your manager nodes (go to EC2 console and explore one manager instance for this)"
+echo "Find public DNS of one of your AWS manager nodes (go to EC2 console and explore one of the manager instance for this)"
 echo "#################################################################################################################"
 echo -n "Type the public DNS of your manger node, followed by [ENTER]:"
 read mangerdns
+echo "Find public DNS of the load balancer (go to EC2 console and find details of the load balancer)"
+echo "#################################################################################################################"
+echo -n "Type the public DNS of your load balancer, followed by [ENTER]:"
+read elbdns
 echo -n "Type the path to your *.pem file, followed by [ENTER] (for example: /Users/idugalic/.ssh/idugalic.pem):"
 read keypath
 
 # chmod 400 ~/.ssh/idugalic.pem
 chmod 400 "$keypath"
-echo "Openining the SSH tunel for $mangerdns"
+
+echo "Copy the docker compose file to AWS manager node"
 echo "######################################"
-# ssh -NL localhost:2374:/var/run/docker.sock -i ~/.ssh/idugalic.pem docker@ec2-35-156-14-194.eu-central-1.compute.amazonaws.com &
-ssh -NL localhost:2374:/var/run/docker.sock -i $keypath $mangerdns &
+# scp -i ~/.ssh/idugalic.pem docker-compose-v3.yml docker@ec2-35-156-120-226.eu-central-1.compute.amazonaws.com:~
+scp -i $keypath -oStrictHostKeyChecking=no docker-compose-v3.yml docker@$mangerdns:~
+echo "SSH to $mangerdns and deploy"
+echo "######################################"
+# ssh -i ~/.ssh/idugalic.pem -oStrictHostKeyChecking=no docker@ec2-35-156-120-226.eu-central-1.compute.amazonaws.com
+ssh -i $keypath -oStrictHostKeyChecking=no docker@$mangerdns "docker stack deploy --compose-file docker-compose-v3.yml micro-company"
 
-export DOCKER_HOST=localhost:2374
-echo "Deploying the bundle"
-echo "####################"
+echo "Visit in your browser: http://$elbdns:9000/socket/index.html"
+echo "Create blogpost from console: $ curl -H "Content-Type: application/json" -X POST -d '{"title":"xyz","rawContent":"xyz","publicSlug": "publicslug","draft": true,"broadcast": true,"category": "ENGINEERING", "publishAt": "2016-12-23T14:30:00+00:00"}' http://$elbdns:9000/command/blog/blogpostcommands"
+echo "Query blogposts from console: $ curl http://$elbdns:9000/query/blog/blogposts"
 
-docker stack deploy --compose-file docker-compose-v3.yml micro-company
