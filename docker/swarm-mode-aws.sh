9b3d2e3575e52ad0b80f5a41fcaec667489baf50
Ivan Dugalic <idugalic@gmail.com>
2016-12-03 00:08:29 +0100

    Updating docker aws swarm shell script

--- docker/swarm-mode-aws.sh
+++ docker/swarm-mode-aws.sh
@@ -11,14 +11,18 @@ echo "Step2:  Create Your Key Pair Using Amazon EC2. Please not that the key wil
 echo "Step3:  Create stack on AWS by using CloudFormation template - https://blog.docker.com/2016/11/docker-aws-public-beta/ (https://console.aws.amazon.com/cloudformation/home#/stacks/new?stackName=Docker&templateURL=https://docker-for-aws.s3.amazonaws.com/aws/beta/aws-v1.13.0-rc2-beta12.json)"
 echo "-----------------------------------------------------------------------------------------------------------------------------"
 
-echo "Find public DNS of one of your AWS manager nodes (go to EC2 console and explore one of the manager instance for this)"
 echo "#################################################################################################################"
-echo -n "Type the public DNS of your manger node, followed by [ENTER]:"
+echo "Find public DNS of one of your AWS manager nodes (go to AWS EC2 console to find the details)"
+echo "#################################################################################################################"
+echo -n "Type the public DNS of your AWS manger node, followed by [ENTER]:"
 read mangerdns
-echo "Find public DNS of the load balancer (go to EC2 console and find details of the load balancer)"
+
 echo "#################################################################################################################"
-echo -n "Type the public DNS of your load balancer, followed by [ENTER]:"
+echo "Find public DNS of the AWS load balancer (go to AWS EC2 console to find the details)"
+echo "#################################################################################################################"
+echo -n "Type the public DNS of the AWS load balancer, followed by [ENTER]:"
 read elbdns
+
 echo -n "Type the path to your *.pem file, followed by [ENTER] (for example: /Users/idugalic/.ssh/idugalic.pem):"
 read keypath
 
@@ -34,7 +38,13 @@ echo "######################################"
 # ssh -i ~/.ssh/idugalic.pem -oStrictHostKeyChecking=no docker@ec2-35-156-120-226.eu-central-1.compute.amazonaws.com
 ssh -i $keypath -oStrictHostKeyChecking=no docker@$mangerdns "docker stack deploy --compose-file docker-compose-v3.yml micro-company"
 
-echo "Visit in your browser: http://$elbdns:9000/socket/index.html"
-echo "Create blogpost from console: $ curl -H "Content-Type: application/json" -X POST -d '{"title":"xyz","rawContent":"xyz","publicSlug": "publicslug","draft": true,"broadcast": true,"category": "ENGINEERING", "publishAt": "2016-12-23T14:30:00+00:00"}' http://$elbdns:9000/command/blog/blogpostcommands"
-echo "Query blogposts from console: $ curl http://$elbdns:9000/query/blog/blogposts"
+echo "############### Track Web socket messages in browser #######################"
+echo "http://$elbdns:9000/socket/index.html"
+echo "################## Create Blog Posts ####################"
+echo "$ curl -H \"Content-Type: application/json\" -X POST -d '{\"title\":\"xyz\",\"rawContent\":\"xyz\",\"publicSlug\": \"publicslug\",\"draft\": true,\"broadcast\": true,\"category\": \"ENGINEERING\", \"publishAt\": \"2016-12-23T14:30:00+00:00\"}' http://$elbdns:9000/command/blog/blogpostcommands"
+echo "################## Query Blog Posts ####################"
+echo "$ curl http://$elbdns:9000/query/blog/blogposts"
+echo "######################################"
+echo "View logs on AWS Cloud Watch"
+echo "Have fun !!!"
 
