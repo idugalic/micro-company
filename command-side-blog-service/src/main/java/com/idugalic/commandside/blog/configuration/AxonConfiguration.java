01accd9f83895e9f21a7848f614f7aff94e6e05c
Ivan Dugalic <idugalic@gmail.com>
2016-12-16 15:54:03 +0100

    Adding bean validation interceptor for Axon command bus.

---
+++ command-side-blog-service/src/main/java/com/idugalic/commandside/blog/configuration/AxonConfiguration.java
@@ -0,0 +1,28 @@
+package com.idugalic.commandside.blog.configuration;
+
+import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.commandhandling.SimpleCommandBus;
+import org.axonframework.common.transaction.TransactionManager;
+import org.axonframework.messaging.interceptors.BeanValidationInterceptor;
+import org.axonframework.monitoring.NoOpMessageMonitor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * A configuration for axonframework.
+ * Axonframework is used to support eventsourcing and CQRS.
+ * 
+ * @author idugalic
+ *
+ */
+@Configuration
+public class AxonConfiguration {
+    
+    @Bean
+    CommandBus commandBus(TransactionManager transactionManager) {
+        SimpleCommandBus commandBus = new SimpleCommandBus(transactionManager, NoOpMessageMonitor.INSTANCE);
+        commandBus.registerDispatchInterceptor(new BeanValidationInterceptor());
+        return commandBus;
+    }
+}
