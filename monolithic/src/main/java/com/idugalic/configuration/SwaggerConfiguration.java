f40052205a6cc0b2063225a4526b5fd69f138cbd
Ivan Dugalic <idugalic@gmail.com>
2016-12-18 19:03:15 +0100

    Additional swagger configuration

--- monolithic/src/main/java/com/idugalic/configuration/SwaggerConfiguration.java
+++ monolithic/src/main/java/com/idugalic/configuration/SwaggerConfiguration.java
@@ -2,12 +2,17 @@ package com.idugalic.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
 
 /**
  * Swagger 2 configuration.
@@ -17,13 +22,28 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
-public class SwaggerConfiguration {                                    
+@Import({springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration.class})
+public class SwaggerConfiguration {
     @Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
+    public Docket api2() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                 .apis(RequestHandlerSelectors.basePackage("com.idugalic"))                
+                .paths(PathSelectors.any())
+                .build();
     }
+    
+    
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Micro-Company API")
+                .description("This version of the application is deployed as a single monolithic application. Domain Driven Design is applied through Event Sourcing and CQRS. How Event Sourcing enables deployment flexibility - the application can be migrated and deployed as a microservices (checkout the master branch).")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://github.com/idugalic/micro-company/blob/master/LICENSE")
+                .version("0.0.1-SNAPSHOT")
+                .build();
+    }
+
+
 }
--- query-side-project/src/main/java/com/idugalic/queryside/project/repository/ProjectRepository.java
+++ query-side-project/src/main/java/com/idugalic/queryside/project/repository/ProjectRepository.java
@@ -2,6 +2,9 @@ package com.idugalic.queryside.project.repository;
 
 import com.idugalic.queryside.project.domain.Project;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
@@ -12,4 +15,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  */
 @RepositoryRestResource(collectionResourceRel = "projects", path = "projects")
 public interface ProjectRepository extends ReadOnlyPagingAndSortingRepository {
+
+    Page<Project> findByCategory(@Param("category") String category, Pageable pageable);
 }
