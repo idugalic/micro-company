f468c9fbb7bacf45725ad4679defa3d017c52239
Ivan Dugalic <idugalic@gmail.com>
2017-05-16 11:37:15 +0200

    Upgrading libraries (spring, axon)

--- api-gateway/src/main/java/com/idugalic/apigateway/Application.java
+++ api-gateway/src/main/java/com/idugalic/apigateway/Application.java
@@ -2,17 +2,20 @@ package com.idugalic.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableZuulProxy
 @EnableResourceServer
 @EnableHystrix
@@ -20,6 +23,20 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return new AuditorAware<String>() {
+            public String getCurrentAuditor() {
+                Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+                if (authentication == null || !authentication.isAuthenticated()) {
+                    return null;
+                }
+
+                return authentication.getName();
+            }
+        };
     }
 
     @Configuration
